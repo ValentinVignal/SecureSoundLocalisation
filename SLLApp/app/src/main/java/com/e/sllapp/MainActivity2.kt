543fb5df7727230e3491f5be975941f787eba6dc
Valentin <valentin.vignal@gmail.com>
2019-09-22 03:47:34 +0800

    Workable SSL2

--- SLLApp/app/src/main/java/com/e/sllapp/MainActivity2.kt
+++ SLLApp/app/src/main/java/com/e/sllapp/MainActivity2.kt
@@ -21,7 +21,6 @@ import com.e.sllapp.R
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
 
-@SuppressLint("Registered")
 class MainActivity2 : AppCompatActivity() {
 
     private var mediaRecorder: MediaRecorder? = null
@@ -35,38 +34,60 @@ class MainActivity2 : AppCompatActivity() {
     private var recorder: AudioRecord? = null
     private var recordingThread: Thread? = null
 
-    private val BufferElements2Rec: Int = 1024 // want to play 2048 (2K) since 2 bytes we use only 1024
+    private val BufferElements2Rec: Int =
+        1024 // want to play 2048 (2K) since 2 bytes we use only 1024
     private var BytesPerElement: Int = 2 // 2 bytes in 16bit format
-    private var bufferSize: Int= AudioRecord.getMinBufferSize(recorderSampleRate,
-        recorderChannels, recorderAudioEncoding)
+    private var bufferSize: Int = AudioRecord.getMinBufferSize(
+        recorderSampleRate,
+        recorderChannels, recorderAudioEncoding
+    )
 
-    private val rootDirectory: File = File(Environment.getExternalStorageDirectory().absolutePath + "/SSL")
+    private val rootDirectory: File =
+        File(Environment.getExternalStorageDirectory().absolutePath + "/SSL")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            val permissions = arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
-            ActivityCompat.requestPermissions(this, permissions,0)
+        setContentView(R.layout.activity_main2)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val permissions = arrayOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+            ActivityCompat.requestPermissions(this, permissions, 0)
         }
-        if (!rootDirectory.exists()){
+        if (!rootDirectory.exists()) {
             rootDirectory.mkdirs()
         }
 
         button_start_recording.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                val permissions = arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
-                ActivityCompat.requestPermissions(this, permissions,0)
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                ) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                val permissions = arrayOf(
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                ActivityCompat.requestPermissions(this, permissions, 0)
             } else {
                 startRecording()
             }
         }
 
-        button_stop_recording.setOnClickListener{
+        button_stop_recording.setOnClickListener {
             stopRecording()
         }
     }
@@ -90,31 +111,33 @@ class MainActivity2 : AppCompatActivity() {
 
 
     private fun startRecording() {
-        recorder = AudioRecord(
-            MediaRecorder.AudioSource.MIC,
-            recorderSampleRate, recorderChannels,
-            recorderAudioEncoding, BufferElements2Rec * BytesPerElement
-        )
-
-        recorder?.let{r ->
-            r.startRecording()
-            isRecording = true
-            recordingThread = Thread(Runnable { writeAudioDataToFile() }, "AudioRecorder Thread")
-        }
-        recordingThread?.start()
-
-        if(!isRecording){
+        if (!isRecording) {
             try {
-                mediaRecorder?.prepare()
-                mediaRecorder?.start()
                 isRecording = true
                 Toast.makeText(this, "Recording started!", Toast.LENGTH_SHORT).show()
                 text_view_state.text = "Recording..."
+                recorder = AudioRecord(
+                    MediaRecorder.AudioSource.MIC,
+                    recorderSampleRate, recorderChannels,
+                    recorderAudioEncoding, BufferElements2Rec * BytesPerElement
+                )
+
+                recorder?.let { r ->
+                    r.startRecording()
+                    isRecording = true
+                    recordingThread =
+                        Thread(Runnable { writeAudioDataToFile() }, "AudioRecorder Thread")
+                }
+                recordingThread?.start()
+
             } catch (e: IllegalStateException) {
                 e.printStackTrace()
             } catch (e: IOException) {
                 e.printStackTrace()
             }
+        } else {
+            Toast.makeText(this, "You are already recording", Toast.LENGTH_SHORT).show()
+
         }
     }
 
@@ -123,7 +146,7 @@ class MainActivity2 : AppCompatActivity() {
 
         var i = 0
         recordPath = rootDirectory.absolutePath + "/recording_$i.pcm"
-        while(File(recordPath).exists()){
+        while (File(recordPath).exists()) {
             i += 1
             recordPath = rootDirectory.absolutePath + "/recording_$i.pcm"
         }
@@ -160,16 +183,23 @@ class MainActivity2 : AppCompatActivity() {
     }
 
 
-    private fun stopRecording(){
+    private fun stopRecording() {
         // stops the recording activity
-        if (null != recorder) {
-            isRecording = false
-            recorder?.let{r ->
-                r.stop()
-                r.release()
+        if (isRecording) {
+            if (null != recorder) {
+                isRecording = false
+                recorder?.let { r ->
+                    r.stop()
+                    r.release()
+                }
+                recorder = null
+                recordingThread = null
+                Toast.makeText(this, "Recording saved in $recordPath", Toast.LENGTH_SHORT).show()
+                isRecording = false
+                text_view_state.text = "Press Start to record"
             }
-            recorder = null
-            recordingThread = null
+        } else {
+            Toast.makeText(this, "You are not recording right now!", Toast.LENGTH_SHORT).show()
         }
     }
 }
