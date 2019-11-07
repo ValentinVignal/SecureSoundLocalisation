b909db2e18461ed8e538c059f596c1d463621a99
Valentin <valentin.vignal@gmail.com>
2019-11-07 16:13:26 +0800

    More robust + put read message in ThreadSpeaker.kt file

--- SSLApp/app/src/main/java/com/e/sslapp/customElements/ThreadSpeaker.kt
+++ SSLApp/app/src/main/java/com/e/sslapp/customElements/ThreadSpeaker.kt
@@ -1,150 +1,75 @@
 package com.e.sslapp.customElements
 
-import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
 import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
-import android.os.Handler
 import android.util.Log
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.beust.klaxon.Klaxon
 import com.beust.klaxon.KlaxonException
 import com.e.sslapp.v4.Activity4Speaker
 import java.util.*
 import kotlin.collections.ArrayList
 
-class ThreadSpeakers(activity: Activity4Speaker): Thread() {
-    val activity = activity
 
-    var currentworker: Thread? = null
-
-    override fun run (){
-        while(true){
-            currentworker = ThreadSpeaker(activity)
-            currentworker?.start()
-            currentworker?.let{
-                while(it.isAlive){
-                    sleep(1000)
-                    println("isAlive")
-                }
-                println("isNotAlive")
-            }
-
-        }
-    }
-
-    fun terminate(){
-        currentworker?.stop()
-        super.stop()
-    }
-
-    override fun destroy() {
-        currentworker?.destroy()
-        super.stop()
-    }
-
-}
-
-class ThreadSpeaker(activity: Activity4Speaker) :
-    Thread() {
-
-    val activity = activity
-
-    override fun run() {
-        println("in run ThreadSpeaker start")
-        activity.speaker()
-        println("in run ThreadSpeaker end")
-    }
-}
-
-class ThreadPlaySound(activity: Activity4Speaker, track: AudioTrack): Thread(){
-    val activity = activity
-    val track = track
-
-    override fun run() {
-        activity.playSoundHandled(track)
-    }
-}
-
-class BluetoothSpeakers2(socket: BluetoothSocket, sampleRate: Int, activity:Activity4Speaker): Thread() {
+class ThreadSpeakers(socket: BluetoothSocket, sampleRate: Int, activity:Activity4Speaker): Thread() {
     val activity = activity
     val sampleRate = sampleRate
     val socket = socket
+
+    var running: Boolean = true
+    var currentWorker: Thread? = null
 
     override fun run(){
-        while(true){
-            val thread = BluetoothSpeaker2(socket, sampleRate, activity)
-            thread.start()
-            while(thread.isAlive){
-                sleep(1000)
+        try{
+            while(! interrupted()){
+                println("is Alive")
+                println("while running: $running")
+                currentWorker = ThreadSpeaker(socket, sampleRate, activity)
+                currentWorker?.start()
+                currentWorker?.let{itCurrentWorker ->
+                    while(itCurrentWorker.isAlive){
+                        sleep(1000)
+                    }
+                }
             }
+        } catch (e: InterruptedException){
+            currentWorker?.interrupt()
+            currentThread().interrupt()
+            Log.d("ThreadSpeakers", "interrupted")
         }
     }
-
 }
 
-class BluetoothSpeaker2(socket: BluetoothSocket, sampleRate: Int, activity:Activity4Speaker): Thread() {
+class ThreadSpeaker(socket: BluetoothSocket, sampleRate: Int, activity:Activity4Speaker): Thread() {
     val activity = activity
     val sampleRate = sampleRate
     val socket = socket
 
     override fun run() {
-        println("in run BluetoothSpeaker2")
-        val (startPlay, soundToPlay) = readSoundMessage()
-        startPlay?.let{itStartPlay ->
-            soundToPlay?.let{itSoundToPlay ->
-                println("in both let")
-                activity.updateGraphSound(itSoundToPlay)
-                val datePlayed = playSound(itStartPlay, itSoundToPlay)
-                sendDatePlayed(datePlayed)
-            }
-        }
-    }
-
-    fun readMessage(): String{
         try{
-            var available = socket.inputStream?.available()
-            while(available == 0){
-                sleep(200)
-                available = socket.inputStream?.available()
-                println()
-            }
-            var text = ""
-            while (available != 0){
-                available?.let{ itAvailable ->
-                    val bytes = ByteArray(itAvailable)
-                    Log.i("readMessage", "Reading")
-                    socket.inputStream?.read(bytes, 0, itAvailable)
-                    Log.i("readMessage", "InputStream ${socket.inputStream}")
-                    Log.i("readMessage", "available $itAvailable")
-                    text += String(bytes)
-                    Log.i("readMessage", "Message received: text $text")
-                    sleep(100)
-                    available = socket.inputStream?.available()
+            val (startPlay, soundToPlay) = readSoundMessage()
+            startPlay?.let{itStartPlay ->
+                soundToPlay?.let{itSoundToPlay ->
+                    println("in both let")
+                    activity.updateGraphSound(itSoundToPlay)
+                    val datePlayed = playSound(itStartPlay, itSoundToPlay)
+                    sendDatePlayed(datePlayed)
                 }
             }
-            return text
-        } catch (e: java.lang.Exception){
-            Log.e("readMessage", "Cannot read data", e)
-            return e.toString()
-        } catch (e: java.lang.NullPointerException){
-            Log.e("readMessage", "Input Stream not available", e)
-            return e.toString()
+        } catch (e: InterruptedException){
+            currentThread().interrupt()
+            Log.d("ThreadSpeaker", "Interrupted")
         }
-        val text = "Didn't get any text"
-        Log.e("readMessage", text)
-        return text
     }
 
+
+
     private fun readSoundMessage(): Pair<Long?, ArrayList<Short>?>{
-        Log.d("ReadSoundMessage", "in function")
         activity.setTextViewState("Getting the sound to play...")
         try {
-            val message = readMessage()
+            val message = readMessage(socket)
             val messageJSON = Klaxon().parse<BluetoothSpeakerSound>(message)
-            Log.d("ReadSoundMessage", "Klaxon OK")
             val startPlay = messageJSON?.start
             val soundToPlay = messageJSON?.sound
             activity.setTextStartPlay(startPlay.toString())
@@ -188,6 +113,39 @@ class BluetoothSpeaker2(socket: BluetoothSocket, sampleRate: Int, activity:Activ
     }
 }
 
+
+
+fun readMessage(socket: BluetoothSocket): String{
+    try{
+        var available = socket.inputStream?.available()
+        while(available == 0){
+            Thread.sleep(200)
+            available = socket.inputStream?.available()
+            println()
+        }
+        var text = ""
+        while (available != 0){
+            available?.let{ itAvailable ->
+                val bytes = ByteArray(itAvailable)
+                Log.i("readMessage", "Reading")
+                socket.inputStream?.read(bytes, 0, itAvailable)
+                Log.i("readMessage", "InputStream ${socket.inputStream}")
+                Log.i("readMessage", "available $itAvailable")
+                text += String(bytes)
+                Log.i("readMessage", "Message received: text $text")
+                Thread.sleep(100)
+                available = socket.inputStream?.available()
+            }
+        }
+        return text
+    } catch (e: java.lang.Exception){
+        Log.e("readMessage", "Cannot read data", e)
+        return e.toString()
+    } catch (e: java.lang.NullPointerException){
+        Log.e("readMessage", "Input Stream not available", e)
+        return e.toString()
+    }
+}
 
 
 
